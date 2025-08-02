7bdf50e0fed48eaf23e94cca6957e7803f3be25d
TheCotOfTehas <TsebroEvgeny@yandex.ru>
2025-08-02 21:20:55 +0300

    add ManagementApplication dll

---
+++ ManagementApplication/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementApplication
+{
+    public class Class1
+    {
+    }
+}
