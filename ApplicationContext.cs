cee56f13a06ee0a90f093ebb5add9cba2ce9bef7
TheCotOfTehas <TsebroEvgeny@yandex.ru>
2025-08-03 12:37:19 +0300

    add Nuget sqlFramework

---
+++ ApplicationContext.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class ApplicationContext : DbContext
+{
+
+	public ApplicationContext()
+	{
+	}
+}
---
+++ ERPSystemDevelopment/EntityFrameworkCore/ApplicationContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ERPSystemDevelopment.EntityFrameworkCore
+{
+    public class ApplicationContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=testdb;Trusted_Connection=True;");
+        }
+    }
+}
