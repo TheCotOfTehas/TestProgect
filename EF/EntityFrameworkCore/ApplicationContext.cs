041dba9ec31a4ecc6c68c883a71fc8d3eb899c99
TheCotOfTehas <TsebroEvgeny@yandex.ru>
2025-08-03 13:02:40 +0300

    Add folder Infrastructure

--- EF/EntityFrameworkCore/ApplicationContext.cs
+++ EF/EntityFrameworkCore/ApplicationContext.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System;
 
-namespace ManagementApplication.EntityFrameworkCore
+namespace EF.EntityFrameworkCore
 {
     public class ApplicationContext : DbContext
     {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=testdb;Trusted_Connection=True;");
